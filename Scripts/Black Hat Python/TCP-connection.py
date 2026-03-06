6906d48d7127354f9705387ed3546bd1e395ee14
Chief-Ethical-Programmer <74800084+Chief-Ethical-Programmer@users.noreply.github.com>
2026-03-06 19:30:44 +0530

    Updated logic

--- Scripts/Black Hat Python/TCP-connection.py
+++ Scripts/Black Hat Python/TCP-connection.py
@@ -1,16 +1,37 @@
 #! /usr/bin/env python3
 import socket
-target = "www.google.com"
-port = 80
+import argparse
+import sys
 
-s=socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Creatiing an object where AF_INET means IPv4, SOCK_STREAM means TCP
-s.connect((target, port))
-s.send(b"GET / HTTP/1.1\r\nHost: www.google.com\r\nConnection: close\r\n\r\n") #Connection: close is used to close the connection after the response is received
-response = b"" #empty string to store the response
-while True:
-    data = s.recv(4096)
-    if not data: #empty values as false hences breaks the loop
-        break
-    response+=data
-print(response.decode("utf-8"))
-s.close()
+def main():
+    parser =argparse.ArgumentParser(prog="TCP-connection.py", description="CLI based TCP connection client") # initialization of the parser object 
+    parser.add_argument("-t","--target", required=True, help="Target IP address or hostname")
+    parser.add_argument("-p","--port", required=True, type=int, help="Target port")
+
+    args = parser.parse_args() #for taking the arguments
+
+    target_host = args.target
+    target_port = args.port
+
+    print(f"[+] Connecting to {target_host} on port {target_port}.....")
+
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: #Creatiing an object where AF_INET means IPv4, SOCK_STREAM means TCP
+             s.settimeout(5) # 5s wait time than retry
+             s.connect((target_host, target_port))
+             print("[+] Connection Established")
+             request = f"GET / HTTP/1.1\r\nHost: {target_host}\r\n\r\n"
+             s.send(request.encode())
+             response=b""
+             while True:
+                data = s.recv(4096)
+                if not data: #empty values as false hences breaks the loop
+                    break
+                response+=data
+             print(response.decode("utf-8"))
+    except Exception as e:
+        print(f"[-] Connection Failed: {e}")
+        sys.exit(1)
+
+if __name__=="__main__":
+    main()
