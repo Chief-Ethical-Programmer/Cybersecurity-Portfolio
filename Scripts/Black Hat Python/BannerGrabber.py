9f29d3bea5d043db1a0f3a9578b3158ae681e06a
Chief-Ethical-Programmer <74800084+Chief-Ethical-Programmer@users.noreply.github.com>
2026-03-10 10:54:19 +0530

    Adding Comments

--- Scripts/Black Hat Python/BannerGrabber.py
+++ Scripts/Black Hat Python/BannerGrabber.py
@@ -3,26 +3,28 @@ import socket
 import ssl
 import argparse
 
+#To take cli arguments for the target IP address
 parser = argparse.ArgumentParser(description='Banner Grabber')
 parser.add_argument('-t', '--target', required=True, help='Target IP')
 args = parser.parse_args()
 
 IP = args.target
-Ports = [21, 22, 23, 25, 80, 443, 8080]
+Ports = [21, 22, 23, 25, 80, 443, 8080] # All the common ports to grab banners from the host
 
+# Function to grab banners from the specified ports
 def grabber(IP, Ports):
     for Port in Ports:
         try:
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 s.settimeout(5)
                 s.connect((IP, Port))
-                if Port == 443:
+                if Port == 443: # Wrapping the socket with ssl for secure connection 
                     context = ssl.create_default_context()
                     s = context.wrap_socket(s, server_hostname=IP)
-                if Port in [80, 443, 8080]:
+                if Port in [80, 443, 8080]: # Sending HTTP request to grab the banner from the web server
                     request = f"GET / HTTP/1.1\r\nHost: {IP}\r\nConnection: close\r\n\r\n"
                     s.sendall(request.encode())
-                banner = s.recv(1024).decode().strip()
+                banner = s.recv(1024).decode()
                 print(f"Banner for {IP}:{Port}: {banner}")
         except socket.error:
             print(f"Could not connect to {IP}:{Port}")
